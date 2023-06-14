aa2fe79a8cfe9544198ea3c556d818c4b7448d07
bugra <bugraarslan96@gmail.com>
2023-06-14 03:07:07 +0300

    topappbar eklendi topappbar rengi yazıldı
    TODO
    indirilen fontlar projeye dahil edilecek

--- app/src/main/java/com/example/hbacademyapp/ui/home/Home.kt
+++ app/src/main/java/com/example/hbacademyapp/ui/home/Home.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.hbacademyapp.ui.home
 
 import androidx.compose.foundation.Image
@@ -12,28 +14,55 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
 import com.example.hbacademyapp.domain.item.GameItem
+import com.example.hbacademyapp.ui.theme.HbBlue
 
 @Composable
 fun HomeScreen() {
     val homeViewModel = viewModel(modelClass = HomeViewModel::class.java)
     val games by homeViewModel.games.collectAsState()
 
-    LazyColumn {
+    Column {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "GAMES",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    color = White,
+                    fontSize = 20.sp,
+                    )
+            },
+            colors = topAppBarColors(containerColor = HbBlue)
+        )
 
-        items(games) { game: GameItem ->
-            GameCard(game = game)
+        LazyColumn {
+            items(games) { game: GameItem ->
+                GameCard(game = game)
+            }
         }
     }
 }
